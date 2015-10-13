02243052975a745bdd6001ea01a54e41a5309e89
= <=>
2015-10-13 13:43:29 +0100

    Added powershell script to copy to home

---
+++ Copy-VimSettings.ps1
@@ -0,0 +1,3 @@
+copy-item c:\codebase\github\VimSettings\.vimrc ~\.vimrc -force
+copy-item c:\codebase\github\VimSettings\.vim\* ~\vimfiles -force -recurse
+copy-item c:\codebase\github\VimSettings\.gvimrc ~\.gvimrc -force
